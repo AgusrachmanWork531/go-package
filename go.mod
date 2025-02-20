1f4d2a2f31ded5ec1d3181dfccb98d6035915807
agusrachman531 <agusrachman531@gmail.com>
2025-02-20 10:03:00 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AgusrachmanWork531/go-package/puppy
+
+go 1.22.1
---
+++ puppy.go
@@ -0,0 +1,9 @@
+package puppy
+
+func Break() string {
+	return "Woof!!!"
+}
+
+func Breaks() string {
+	return "Woof Woof Woof!!!"
+}
